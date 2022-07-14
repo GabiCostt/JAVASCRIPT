bf16b9a628e662cc75cec5b249d9c2fe73282312
Gabriel Costa Santos <104160744+GabiCostt@users.noreply.github.com>
2022-07-14 18:50:05 -0300

    criei a aula 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,5 @@
+let = 82
+
+if (let > 80) {
+    console.log('Você ulrapassou o limite de velocidade de 80km/h')
+}
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+
+if (pais == 'brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é gringo')
+}
